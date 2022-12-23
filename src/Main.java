dda821c2b59013909338b9e25f81344523ef69d8
Alexander Lukyanov <lux-nova@mail.ru>
2022-12-23 21:51:08 +0300

    a straight solution

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 public class Main {
         static int[] TEST = { 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52 };
 
+
     public static void main(String[] args) {
         build(TEST);
-
-
     }
 
 
     static void mark(int[] arr, int left, int right, int level, int[] levels) {
-//        int maxLevel = level;
         if (left == right) {
             levels[left] = level;
             return;
@@ -18,19 +16,23 @@ public class Main {
         levels[middle] = level;
         mark(arr, left, middle - 1, level + 1, levels);
         mark(arr, middle + 1, right, level + 1, levels);
-//        return Math.max()
     }
 
     static void build(int[] arr) {
         int[] levels = new int[arr.length];
         mark(arr, 0, arr.length - 1, 0, levels);
+        StringBuilder plotter = new StringBuilder();
 
-        for (int level = 0; level < 5 ; level++) {
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(levels[i] == level ?
-                        arr[i] : "  ");
-            }
-            System.out.println();
+        int levelsTotal = (int) Math.ceil(Math.log(arr.length + 1) / Math.log(2));
+
+        for (int level = 0; level < levelsTotal ; level++) {
+            for (int i = 0; i < arr.length; i++)
+                plotter.append(levels[i] == level ? arr[i] : "  ");
+            plotter.append("\n");
         }
+        System.out.println(plotter);
     }
+
+
+
 }
